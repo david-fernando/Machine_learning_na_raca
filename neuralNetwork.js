7f3418a5386ce0004206326f72db52ba3476973d
David Fernando <30445601+david-fernando@users.noreply.github.com>
2019-11-14 21:57:33 -0300

    Add files via upload

--- neuralNetwork.js
+++ neuralNetwork.js
@@ -1,37 +1,37 @@
-let input = [1, 0, 1];
-let weight = [Math.random(), Math.random(), Math.random()];
-let outputExpected = [1, 0, 1];
-const learningRate = 0.5;
-let summation;
+let input = [1, 1];
+let weight = [Math.random(), Math.random()];
+let outputExpected = [1.0];
+const learningRate = 0.50;
+let summation = [];
 let output = [];
 let message;
 let time = 1;
-const timeMaximum = 200;
-let sigmoid = [0,0,0];
-function foreword(){
-	summation = input.map((input, weight) => summation=+(input *  weight));
+const timeMaximum = 10;
+let sigmoid = [];
+function foreword(input, weight){
+	summation = outputExpected.map((index,summation) =>+(input[index] * weight[index]));
 }
-function backpropagation(){
-	weight = weight.map((weight, input) => weight * learningRate * input);
+function backpropagation(weights, input, learningRate){
+	weight = weight.map((weight, index) =>weights[index] * learningRate * input[index]);
 }
-function stepActivation(){
-	output = summation.map((output, summation) => output = summation <= 0 ? output = -1: output = 1); 
+function stepActivation(summations){
+	output = summation.map((summation, outputs, index) => outputs = summations[index] <= 0 ? outputs = -1: outputs = 1); 
 }
-function sigmoidActivation(){
-	sigmoid = sigmoid.map((sigmoid, summation) => 1 /( 1 + Math.pow(Math.E, summation)))
-	derivedFromSigmoid();
+function sigmoidActivation(summation){
+	sigmoid = outputExpected.map((sigmoid) => 1 /( 1 + Math.pow(Math.E, summation)));
+	derivedFromSigmoid(sigmoid, summation);
 }
-function derivedFromSigmoid(){
-	output = sigmoid.map((sigmoid, summation) => parseInt(1 - sigmoid * summation));
+function derivedFromSigmoid(sigmoid, summation){
+	output = sigmoid.map((derived) => parseInt(1 - sigmoid * summation));
 }
 function messages(){
-	console.log(`Time ${time}\nOutput:${output}\nOutput expected:${outputExpected}`)
+	console.log(`Time ${time}\nOutput:${output}\nSomatória:${summation}\nOutput expected:${outputExpected}`)
 }
 function training(){
-	while(time < timeMaximum){
-		foreword();
-		backpropagation();
-		sigmoidActivation();
+	while(time < timeMaximum || parseInt(output) != outputExpected){
+		foreword(input, weight);
+		backpropagation(weight, input, learningRate);
+		sigmoidActivation(summation);
 		messages();
 		time += 1 
 	}
