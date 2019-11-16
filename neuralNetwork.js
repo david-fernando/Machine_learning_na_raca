73a480443a9873a6445fc11a0dd52633809b13a5
David Fernando <30445601+david-fernando@users.noreply.github.com>
2019-11-16 13:53:37 -0300

    Add files via upload

--- neuralNetwork.js
+++ neuralNetwork.js
@@ -1,18 +1,38 @@
-let input = [1, 1];
+let input = [20, 36, 40, 30, 0];
 let weight = [Math.random(), Math.random()];
-let outputExpected = [1];
+let outputExpected = [4];
 const learningRate = 0.50;
 let summation = [];
 let output = [];
+let normalizedInput;
+let normalizedOutput;
+let denormalizedInput;
+let denormalizedOutput;
+let denormalizedExpectedOutput;
 let message;
 let time = 1;
 const timeMaximum = 10;
 let sigmoid = [];
-function foreword(input, weight){
-	summation = outputExpected.map((index,summation) =>+(input[index] * weight[index]));
+function foreword(normalizedInput, weight){
+	summation = normalizedInput.reduce((normalizedInput, index) =>+(normalizedInput * weight[index]));
 }
 function backpropagation(weights, input, learningRate){
 	weight = weight.map((weight, index) =>weights[index] * learningRate * input[index]);
+}
+function normalizeData(input, outputExpected){
+	normalizedInput = input.map((inputNormalization, index) =>{
+		if(input[index]> 0){
+			return input[index] / input[index]
+		}else{
+			return 1;
+		}
+	});
+	normalizedOutput = outputExpected.map((outputNormalization, index) => outputExpected[index] / outputExpected[index]);
+}
+function denormalizeData(input, normalizedInput,outputExpected, output){
+	denormalizedInput = input.map((inputDenormalization, index) => input[index] * normalizedInput[index]);
+	denormalizedOutput = outputExpected.map((outputDenormalization, index) => outputExpected[index] * output[index]);
+	denormalizedExpectedOutput = output.map((expectedOutputDenormalization, index) => outputExpected[index] * output[index]);
 }
 function stepActivation(summations){
 	output = summation.map((summation, outputs, index) => outputs = summations[index] <= 0 ? outputs = -1: outputs = 1); 
@@ -23,15 +43,18 @@ function sigmoidActivation(summation){
 }
 function derivedFromSigmoid(sigmoid, summation){
 	output = sigmoid.map((derived) => parseInt(1 - sigmoid * summation));
+	return output;
 }
 function messages(){
-	console.log(`Time ${time}\nOutput:${output}\nOutput expected:${outputExpected}`)
+	console.log(`Time ${time}\nOutput:${denormalizedExpectedOutput}\nSummation:${summation}\nOutput expected:${outputExpected}`)
 }
 function training(){
-	while(parseInt(output) != outputExpected || time < timeMaximum){
-		foreword(input, weight);
-		backpropagation(weight, input, learningRate);
+	while(parseInt(output) != normalizedOutput || time < timeMaximum){
+		normalizeData(input, outputExpected);
+		foreword(normalizedInput, weight);
+		backpropagation(weight, normalizedInput, learningRate);
 		sigmoidActivation(summation);
+		denormalizeData(input, normalizedInput, outputExpected, output);
 		messages();
 		time += 1 
 	}
